47b7ddfc5144049e060379e673d4fb6a6815cce4
younggil94 <younggil9488@gmail.com>
2019-12-26 20:38:18 +0900

    https와 http 구현 및 세션공유 확인

--- src/main/java/com/boot/BootApplication.java
+++ src/main/java/com/boot/BootApplication.java
@@ -1,12 +1,29 @@
 package com.boot;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
+	}
+	
+	@Bean
+	public ServletWebServerFactory ServerFactory() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(8080);
+		return connector;
 	}
 }
--- src/main/java/com/boot/config/WebSecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.boot.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-	
-}
