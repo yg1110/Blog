faa6aa813b409b575f491e8b635ef0a41b22ddf0
younggil94 <younggil9488@gmail.com>
2020-02-22 20:39:52 +0900

    gitignore 적용

--- target/classes/com/boot/user.sql
+++
@@ -1,8 +0,0 @@
-CREATE SCHEMA `blog` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
-
-INSERT INTO `blog`.`user` (`email`, `pwd`) VALUES ('aaa', 'aaa');
-INSERT INTO `blog`.`user` (`email`, `pwd`) VALUES ('bbb', 'bbb');
-
-use blog;
-select * from user;
-
